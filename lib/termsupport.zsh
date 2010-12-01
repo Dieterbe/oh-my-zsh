Dieterbe/oh-my-zsh

commit c82a1c79e358aa8581fed05d3bfeb3bcdbdc15cf

compress hostname in window title

lib/termsupport.zsh
@@ -1,21 +1,27 @@
+typeset -A host_repr_txt
+# define compact hostnames here. wish I could deduce this from host_repr
+host_repr_txt=('dieter-ws-a7n8x-arch' "ws" 'dieter-p4sci-arch' "p4")
+
+host="@${host_repr_txt[$(hostname)]:-$(hostname)}"
+
 case "$TERM" in
   xterm*|rxvt*)
     preexec () {
-      print -Pn "\e]0;%n@%m: $1\a"  # xterm
+      print -Pn "\e]0;%n${host} $1\a"  # xterm
     }
     precmd () {
-      print -Pn "\e]0;%n@%m: %~\a"  # xterm
+      print -Pn "\e]0;%n${host} %~\a"  # xterm
     }
     ;;
   screen*)
     preexec () {
       local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
       echo -ne "\ek$CMD\e\\"
-      print -Pn "\e]0;%n@%m: $1\a"  # xterm
+      print -Pn "\e]0;%n${host} $1\a"  # xterm
     }
     precmd () {
       echo -ne "\ekzsh\e\\"
-      print -Pn "\e]0;%n@%m: %~\a"  # xterm
+      print -Pn "\e]0;%n${host} %~\a"  # xterm
     }
     ;;
 esac

themes/dieter.zsh-theme
@@ -4,7 +4,6 @@
 # exact return code (when >0) is on the right, so it stays out of the way
 
 # TODO: reset exit code visual cues (not exit code itself) after showing once
-# TODO: compress hostname in window title
 
 typeset -A host_repr
 host_repr=('dieter-ws-a7n8x-arch' "%{$fg_bold[green]%}ws" 'dieter-p4sci-arch' "%{$fg_bold[blue]%}p4")

commit b4c4571bea64212b91ce90e1b256aa8717a14718

Do not override host parameter used in PS1
We cannot name the variable holding the representation of the hostname
for the terminal title the same as the one we use for building PS1.
This bug, introduced in c82a1c79e358aa8581fed05d3bfeb3bcdbdc15cf,
caused the hostname in the PS1 to become uncolored.

lib/termsupport.zsh
@@ -2,26 +2,26 @@ typeset -A host_repr_txt
 # define compact hostnames here. wish I could deduce this from host_repr
 host_repr_txt=('dieter-ws-a7n8x-arch' "ws" 'dieter-p4sci-arch' "p4")
 
-host="@${host_repr_txt[$(hostname)]:-$(hostname)}"
+local hostp="@${host_repr_txt[$(hostname)]:-$(hostname)}"
 
 case "$TERM" in
   xterm*|rxvt*)
     preexec () {
-      print -Pn "\e]0;%n${host} $1\a"  # xterm
+      print -Pn "\e]0;%n${hostp} $1\a"  # xterm
     }
     precmd () {
-      print -Pn "\e]0;%n${host} %~\a"  # xterm
+      print -Pn "\e]0;%n${hostp} %~\a"  # xterm
     }
     ;;
   screen*)
     preexec () {
       local CMD=${1[(wr)^(*=*|sudo|ssh|-*)]}
       echo -ne "\ek$CMD\e\\"
-      print -Pn "\e]0;%n${host} $1\a"  # xterm
+      print -Pn "\e]0;%n${hostp} $1\a"  # xterm
     }
     precmd () {
       echo -ne "\ekzsh\e\\"
-      print -Pn "\e]0;%n${host} %~\a"  # xterm
+      print -Pn "\e]0;%n${hostp} %~\a"  # xterm
     }
     ;;
 esac